9edbe8426f2cc3a2b24a9f3b476ef158498fa1bd
sorinboia <sorinboia@gmail.com>
2021-06-14 10:47:09 +0300

    ocp

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+TAG=ocp
+
 
 (cd frontend/main && npm run build) && \
-(docker build -t sorinboia/arcadia-frontend:v0.1 frontend/main && docker push sorinboia/arcadia-frontend:v0.1) & \
-(docker build -t sorinboia/arcadia-users:v0.1 backend/users && docker push sorinboia/arcadia-users:v0.1) & \
-(docker build -t sorinboia/arcadia-login:v0.1 backend/login && docker push sorinboia/arcadia-login:v0.1) & \
-(docker build -t sorinboia/arcadia-stock_transaction:v0.1 backend/stock_transaction && docker push sorinboia/arcadia-stock_transaction:v0.1) & \
-(docker build -t sorinboia/arcadia-stocks:v0.1 backend/stocks && docker push sorinboia/arcadia-stocks:v0.1) & \
-(docker build -t sorinboia/arcadia-db:v0.1 backend/arcadia-db && docker push sorinboia/arcadia-db:v0.1)
+(docker build -t sorinboia/arcadia-frontend:$TAG frontend/main && docker push sorinboia/arcadia-frontend:$TAG) & \
+(docker build -t sorinboia/arcadia-users:$TAG backend/users && docker push sorinboia/arcadia-users:$TAG) & \
+(docker build -t sorinboia/arcadia-login:$TAG backend/login && docker push sorinboia/arcadia-login:$TAG) & \
+(docker build -t sorinboia/arcadia-stock_transaction:$TAG backend/stock_transaction && docker push sorinboia/arcadia-stock_transaction:$TAG) & \
+(docker build -t sorinboia/arcadia-stocks:$TAG backend/stocks && docker push sorinboia/arcadia-stocks:$TAG) & \
+(docker build -t sorinboia/arcadia-db:$TAG backend/arcadia-db && docker push sorinboia/arcadia-db:$TAG)
