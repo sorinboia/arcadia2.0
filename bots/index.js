00fcb377fb5ac96c2927e6ecf95f49b822d07184
sorinboia <sorinboia@gmail.com>
2022-10-31 15:34:37 +0200

    1

--- bots/index.js
+++ bots/index.js
@@ -2,7 +2,7 @@ const argv = require('yargs').argv;
 
 
 //const targerUrl = argv.url || 'https://arcadia-bfce5397.uksouth.cloudapp.azure.com/';
-const targerUrl = argv.url || 'https://studentx.vltr.nginx-experience.com/';
+const targerUrl = argv.url || 'https://asdasdasd.vltr.nginx-experience.com/';
 
 
 
@@ -10,34 +10,32 @@ const targerUrl = argv.url || 'https://studentx.vltr.nginx-experience.com/';
 
 
 const main = async () => {
-        try {
+    const client = new (require('./puppet'))( {
+        targetUrl: targerUrl,
+        credentials: {
+            username: 'satoshi@bitcoin.com',
+            password: 'bitcoin'
+        },
+        puppetOptions: {
+            headless: true
+        }
+    });
+    try {
+        const delay = 1000;
+        await client.init();
+        await client.login();
+        for(let i=0;i<2;i++) {
+            await client.transaction({crypto: 'ltc',type: 'buy', amount: '3' });
+            await client.wait(delay);
+            await client.transaction({crypto: 'ltc',type: 'sell', amount: '3' });
+            await client.wait(delay);
+        }
 
-
-            const client = new (require('./puppet'))( {
-                targetUrl: targerUrl,
-                credentials: {
-                    username: 'satoshi@bitcoin.com',
-                    password: 'bitcoin'
-                },
-                puppetOptions: {
-                    headless: false
-                }
-            });
-
-            const delay = 1000;
-            await client.init();
-            await client.login();
-            for(let i=0;i<2;i++) {
-                await client.transaction({crypto: 'ltc',type: 'buy', amount: '3' });
-                await client.wait(delay);
-                await client.transaction({crypto: 'ltc',type: 'sell', amount: '3' });
-                await client.wait(delay);
-            }
-
-            await client.close();
-        } catch(e) {            
-            console.log(e);            
-        }          
+        await client.close();
+    } catch(e) {            
+        await client.close();
+        console.log(e);
+    }
 }
 
 
